78b87eb6cb7009d190093f33035bb0865f75b241
sepehr71 <sepehrshakeri7@gmail.com>
2022-10-30 15:32:10 +0330

    adding tictactoe project fundamentals

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+let tictactoe = {
+    items: ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+}
+
 function change(position) {
-    document.getElementById(position).textContent = 'x'
+
+    document.getElementById(position).textContent = 'x';
+    tictactoe.items[position] = 'x';
+    console.log(tictactoe.items);
+    judge();
+    cpuAction();
+}
+
+function cpuAction() {
+
+    for (let item in tictactoe.items) {
+        if (tictactoe.items[item] === '-') {
+            document.getElementById(item).textContent = 'o';
+            tictactoe.items[item] = 'o';
+            return;
+        }
+    }
+    judge();
+}
+function judge() {
+
 }
