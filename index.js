87c5294a7401305b913bb53da6fded19cf3dc76e
sepehr71 <sepehrshakeri7@gmail.com>
2022-11-02 17:04:40 +0330

    creting score board & judgment + fixing some issues

--- index.js
+++ index.js
@@ -1,30 +1,71 @@
 let tictactoe = {
-    items: ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    items: ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    isFinished: false
 }
 
 function change(position) {
+    if (tictactoe.items[position] === '-' && tictactoe.isFinished === false) {
+        document.getElementById(position).textContent = 'x';
+        tictactoe.items[position] = 'x';
+        judge();
+        cpuAction();
+    }
+}
 
-    document.getElementById(position).textContent = 'x';
-    tictactoe.items[position] = 'x';
-    console.log(tictactoe.items);
-    judge();
-    cpuAction();
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function canyouwin() {
+
 }
 
 function cpuAction() {
 
+    randomfill();
+    canyouwin();
+    canoppwin();
+    trytowin();
+
     for (let item in tictactoe.items) {
-        if (tictactoe.items[item] === '-') {
+        if (tictactoe.items[item] === '-' && tictactoe.isFinished === false) {
             document.getElementById(item).textContent = 'o';
             tictactoe.items[item] = 'o';
+            judge();
             return;
         }
     }
-    judge();
 }
 function judge() {
-    if (tictactoe.items[0] === 'x' && tictactoe.items[0] === tictactoe.items[3] && tictactoe.items[3] === tictactoe.items[6]) {
-
+    if (tictactoe.items[0] !== '-' && tictactoe.items[0] === tictactoe.items[1] && tictactoe.items[1] === tictactoe.items[2]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[0];
+        tictactoe.isFinished = true;
     }
-    if (tictactoe.item[0] === 'x' && tictactoe.items[0] === tictactoe.items[1] && tictactoe.items[1] === tictactoe.items[2])
+    if (tictactoe.items[3] !== '-' && tictactoe.items[3] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[5]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[3];
+        tictactoe.isFinished = true;
+    }
+    if (tictactoe.items[6] !== '-' && tictactoe.items[6] === tictactoe.items[7] && tictactoe.items[7] === tictactoe.items[8]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[6];
+        tictactoe.isFinished = true;
+    }
+    if (tictactoe.items[0] !== '-' && tictactoe.items[0] === tictactoe.items[3] && tictactoe.items[3] === tictactoe.items[6]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[0];
+        tictactoe.isFinished = true;
+    }
+    if (tictactoe.items[1] !== '-' && tictactoe.items[1] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[7]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[1];
+        tictactoe.isFinished = true;
+    }
+    if (tictactoe.items[2] !== '-' && tictactoe.items[2] === tictactoe.items[5] && tictactoe.items[5] === tictactoe.items[8]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[2];
+        tictactoe.isFinished = true;
+    }
+    if (tictactoe.items[0] !== '-' && tictactoe.items[0] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[8]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[0];
+        tictactoe.isFinished = true;
+    }
+    if (tictactoe.items[2] !== '-' && tictactoe.items[2] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[6]) {
+        document.getElementById("score-winner").textContent = tictactoe.items[2];
+        tictactoe.isFinished = true;
+    }
 }
