b4b06391b614e7762a8f23c7ef813fd374813d57
sepehr71 <sepehrshakeri7@gmail.com>
2022-11-05 15:36:10 +0330

    completting judge method.

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function cpuAction() {
 
     // canYouWin();
     // canOppWin();
-
     for (let item in tictactoe.items) {
         if (tictactoe.items[item] === '-' && tictactoe.isFinished === false) {
             document.getElementById(item).textContent = 'o';
@@ -33,58 +32,36 @@ function cpuAction() {
         }
     }
 }
-function judge() {
-    const steps = {
-        step1: { diff: 1, start: [0, 3, 6] },
-        step2: { diff: 2, start: [2] },
-        step3: { diff: 3, start: [0, 1, 2] },
-        step4: { diff: 4, start: [4] }
-    }
 
-    for (let step in steps) {
-        for (let i = 0; i < 3; i++) {
-            console.log('d');
-            if (tictactoe.items[step.start[i]] !== '-' && tictactoe.items[step.start[i]] === tictactoe.items[step.start[i] + step.diff] && tictactoe.items[step.start[i] + step.diff] === tictactoe.items[step.start[i] + step.diff + step.diff]) {
-                console.log('ff');
-                document.getElementById("score-winner").textContent = tictactoe.items[step.start[i]];
+function judge() {
+    const steps = [
+        { diff: 1, start: [0, 3, 6] },
+        { diff: 2, start: [2] },
+        { diff: 3, start: [0, 1, 2] },
+        { diff: 4, start: [4] }
+    ]
+
+    steps.forEach(step => {
+        for (let key in step.start) {
+            if (tictactoe.items[step.start[key]] !== '-' && tictactoe.items[step.start[key]] === tictactoe.items[step.start[key] + step.diff] && tictactoe.items[step.start[key] + step.diff] === tictactoe.items[step.start[key] + step.diff + step.diff]) {
+                document.getElementById("score-winner").textContent = tictactoe.items[step.start[key]];
                 tictactoe.isFinished = true;
             }
         }
-    }
+    })
 
 
 
 
-    // if (tictactoe.items[0] !== '-' && tictactoe.items[0] === tictactoe.items[1] && tictactoe.items[1] === tictactoe.items[2]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[0];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[3] !== '-' && tictactoe.items[3] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[5]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[3];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[6] !== '-' && tictactoe.items[6] === tictactoe.items[7] && tictactoe.items[7] === tictactoe.items[8]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[6];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[0] !== '-' && tictactoe.items[0] === tictactoe.items[3] && tictactoe.items[3] === tictactoe.items[6]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[0];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[1] !== '-' && tictactoe.items[1] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[7]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[1];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[2] !== '-' && tictactoe.items[2] === tictactoe.items[5] && tictactoe.items[5] === tictactoe.items[8]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[2];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[0] !== '-' && tictactoe.items[0] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[8]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[0];
-    //     tictactoe.isFinished = true;
-    // }
-    // if (tictactoe.items[2] !== '-' && tictactoe.items[2] === tictactoe.items[4] && tictactoe.items[4] === tictactoe.items[6]) {
-    //     document.getElementById("score-winner").textContent = tictactoe.items[2];
-    //     tictactoe.isFinished = true;
+
+
+    // for (let step in steps) {
+    //     for (let s in step.start) {
+    //         if (tictactoe.items[step.start[s]] !== '-' && tictactoe.items[step.start[s]] === tictactoe.items[step.start[s] + step.diff] && tictactoe.items[step.start[s] + step.diff] === tictactoe.items[step.start[s] + step.diff + step.diff]) {
+    //             document.getElementById("score-winner").textContent = tictactoe.items[step.start[s]];
+    //             tictactoe.isFinished = true;
+    //         }
+    //     }
     // }
 }
+
